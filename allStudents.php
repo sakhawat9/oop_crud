e61a2cd3f17e1bcdb83f6aa5bd86cb531ad09497
sakhawat9 <sakhawathossain7969@gmail.com>
2023-12-20 07:19:33 +0600

    update

--- allStudents.php
+++ allStudents.php
@@ -25,12 +25,12 @@ if (isset($_GET['delStd'])) {
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#">OOP Crud</a>
+            <a class="navbar-brand" href="index.php">OOP Crud</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav mr-auto">
+                <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="index.php">Home</a>
                     </li>
